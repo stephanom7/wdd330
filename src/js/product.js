1998bb5548125cfddf6e16af372761ee2f054555
stephanom7 <smartinez6@byupathway.edu>
2026-09-12 13:43:05 -0500

    Fix linting quotes

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,11 @@
-import { setLocalStorage, getLocalStorage } from "./utils.mjs";
-import ProductData from "./ProductData.mjs";
+import { setLocalStorage, getLocalStorage } from './utils.mjs';
+import ProductData from './ProductData.mjs';
 
-const dataSource = new ProductData("tents");
+const dataSource = new ProductData('tents');
 
 function addProductToCart(product) {
   // 1. Obtener el carrito actual desde localStorage o inicializar un arreglo vacío si no hay datos
-  let cart = getLocalStorage("so-cart");
+  let cart = getLocalStorage('so-cart');
   if (!Array.isArray(cart)) {
     cart = [];
   }
@@ -14,7 +14,7 @@ function addProductToCart(product) {
   cart.push(product);
 
   // 3. Guardar el arreglo actualizado
-  setLocalStorage("so-cart", cart);
+  setLocalStorage('so-cart', cart);
 }
 
 // add to cart button event handler
@@ -25,5 +25,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById("addToCart")
-  .addEventListener("click", addToCartHandler);
+  .getElementById('addToCart')
+  .addEventListener('click', addToCartHandler);
