493d48ff5c933dab0ae4436feaf41c2b79f1a8b7
stephanom7 <smartinez6@byupathway.edu>
2026-09-12 13:42:11 -0500

    Fix cart overwrite issue by storing items in an array

--- src/js/product.js
+++ src/js/product.js
@@ -1,11 +1,22 @@
-import { setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { setLocalStorage, getLocalStorage } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
 
 function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
+  // 1. Obtener el carrito actual desde localStorage o inicializar un arreglo vacío si no hay datos
+  let cart = getLocalStorage("so-cart");
+  if (!Array.isArray(cart)) {
+    cart = [];
+  }
+
+  // 2. Agregar el nuevo producto al arreglo
+  cart.push(product);
+
+  // 3. Guardar el arreglo actualizado
+  setLocalStorage("so-cart", cart);
 }
+
 // add to cart button event handler
 async function addToCartHandler(e) {
   const product = await dataSource.findProductById(e.target.dataset.id);
@@ -14,5 +25,5 @@ async function addToCartHandler(e) {
 
 // add listener to Add to Cart button
 document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+  .getElementById("addToCart")
+  .addEventListener("click", addToCartHandler);
